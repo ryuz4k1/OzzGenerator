8192b61948d020c526cf26331e4f63dbc585fb11
ryuz4k1 <ozerr.murat@hotmail.com>
2019-12-13 14:47:19 +0300

    take root directory path

---
+++ OzzScript.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "Please provide the path to the directory where you will create the project: "
+echo "Example: Desktop/NodeJS/myProject (Should not begin with /)"
+read projectRootDirectory
+echo "Your directory: " ~/$projectRootDirectory
