54b39c43f4b3de4c881feb188ae2186307b3630c
ryuz4k1 <ozerr.murat@hotmail.com>
2019-12-14 03:24:17 +0300

    files created timeout shape methods done

--- OzzScript.sh
+++ OzzScript.sh
@@ -1,19 +1,27 @@
 #! /bin/bash
 echo "WELCOME TO OZZGENERATOR NODEJS ARCHITECTURE CREATOR"
 
-
-setUpNew () {
- echo "Set up new function"
- echo '
+doInfoShape(){
+  echo '
  -----------------------------
  +                           +
  +           INFO            +
  +                           +
  ----------------------------- '
+}
+
+timeout() {
+  sleep 3
+}
+
+setUp () {
+ echo "Set up new function"
+ doInfoShape
  echo "Please provide the path to the directory where you will create the project: " 
  echo "Example: Desktop/NodeJS/myProject (Should not begin and should not end with '/')"
  echo "The path must be end of your project folder name"
  read projectRootDirectory
+ timeout
  fullPath=~/$projectRootDirectory
  echo "Your directory: " $fullPath
  IFS='/' #delimiter
@@ -23,52 +31,38 @@ setUpNew () {
  cd "$withOutProjectFolder"
  mkdir $projectFolder
  cd $projectFolder
- echo '
- -----------------------------
- +                           +
- +           INFO            +
- +                           +
- ----------------------------- '
- echo "Project created"
+ doInfoShape
+ echo "Project creating"
  echo "You are here now: " "${ADDR[*]}"
- declare -a ROOT_FOLDERS
- declare -a SRC_SUB_FOLDERS
- declare -a PUBLIC_SUB_FOLDERS
- ROOT_FOLDERS=(bin src public test)
- SRC_SUB_FOLDERS=(controllers helpers middleware models views api)
- PUBLIC_SUB_FOLDERS=(js images stylesheets)
- for i in ${ROOT_FOLDERS[@]}
- do
-  	if[$i == "bin"];
-		mkdir bin
-		cd bin
-		touch www
-		cd ..
-  	if[$i == "src"];
-		mkdir src
-		cd src
-  		for j in ${SRC_SUB_FOLDERS[@]}
-  		do
-		 mkdir $j
-        	done
-		cd ..
-  	if[$i == "public"];
-		mkdir public
-        	cd public
-        	for j in ${PUBLIC_SUB_FOLDERS[@]}
-        	do
-        	 mkdir $j
-        	done
-		cd ..
-  	if[$i == "test"];
-		mkdir test
-		echo "Test"
- done
+ mkdir bin
+ cd bin
+ touch www
+ cd ..
+ mkdir src
+ cd src
+ touch app.js
+ mkdir controllers
+ mkdir helpers
+ mkdir models
+ mkdir views
+ mkdir middleware
+ mkdir api
+ cd ..
+ mkdir public
+ cd public
+ mkdir js
+ mkdir images
+ mkdir stylesheets
+ cd ..
+ touch .env
+ touch .gitignore
+ touch config.json
+ touch README.md
+ timeout
+ echo "$PWD"
+ eval "npm init -y"
 }
 
-setUpExisting(){
- "Set up existing function"
-}
 
 
 PS3='Please enter your choice: '
@@ -76,14 +70,15 @@ options=("Set up a new Node.JS project" "Set up an existing Node.JS project" "Qu
 select opt in "${options[@]}"
 do
     case $opt in
-        "Set up a new Node.JS project")
+        "Set up a new Node.JS project folder")
             echo "you chose choice 1"
-	    setUpNew
-	    break
+	          setUp
+	          break
             ;;
-        "Set up an existing Node.JS project")
+        "Set up an existing Node.JS project folder")
             echo "you chose choice 2"
-	    setUpExisting
+	          setUp
+            break
             ;;
         "Quit")
             break
@@ -91,11 +86,3 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
-
-
-<<COMMENT
-read projectRootDirectory
-mkdir $full              
-cd $fullPath
-echo "Your here now: " $pwd
-COMMENT
